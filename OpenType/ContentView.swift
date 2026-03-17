720ce25769194ab317155bd93b6b4fa627a48a30
Hamza Bin Mubeen <80984531+HamzaBinMubeen@users.noreply.github.com>
2026-03-17 23:08:54 +0500

    Initial Commit

---
+++ OpenType/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenType
+//
+//  Created by Hamza Bin Mubeen on 17/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenType/OpenTypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenTypeApp.swift
+//  OpenType
+//
+//  Created by Hamza Bin Mubeen on 17/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct OpenTypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
